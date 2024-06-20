cecbdb8c8adf6bbbf9192ba1bd6177af557f8f38
aphackertg07 <147715203+aphackertg07@users.noreply.github.com>
2024-06-20 22:38:04 +0530

    Update

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ["TOKEN"] = "6105833544:AAG9Ulo_1Mtu2oBQACqK_94ChvB-7L9xrBc"
+
+os.environ["REDIS_URI"] = "redis-17433.c321.us-east-1-2.ec2.cloud.redislabs.com:17433"
+
+os.environ["REDIS_PASSWORD"] = "tGwsCs7bRSB4MFuomgJ2z9xL3b22Grkh"
+
+os.environ["NAME"] = "appromo"
